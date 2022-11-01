4aba7bf48a43d6ee634509dbedcc78516d7c91c4
8366419@qq.com <8366419@qq.com>
2022-11-01 19:49:11 +0800

    18-1

---
+++ src/main/java/llb/tdd/di/ContextConfig.java
@@ -0,0 +1,59 @@
+package llb.tdd.di;
+
+import java.util.*;
+
+import static java.util.List.of;
+
+public class ContextConfig {
+    private Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
+
+    public <Type> void bind(Class<Type> type, Type instance) {
+        providers.put(type, new ComponentProvider<Type>() {
+
+            @Override
+            public Type get(Context context) {
+                return instance;
+            }
+
+            @Override
+            public List<Class<?>> getDependencies() {
+                return of();
+            }
+        });
+    }
+
+    public <Type, Implementation extends Type> void bind(Class<Type> type, Class<Implementation> implementation) {
+        providers.put(type, new ConstructorInjectionProvider(implementation));
+    }
+
+    public Context getContext() {
+        providers.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
+        return new Context() {
+            @Override
+            public <Type> Optional<Type> get(Class<Type> type) {
+                return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get(this));
+            }
+        };
+    }
+
+    private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
+        for (Class<?> dependency : providers.get(component).getDependencies()) {
+            if (!providers.containsKey(dependency)) {
+                throw new DependencyNotFoundException(component, dependency);
+            }
+            if (visiting.contains(dependency)) {
+                throw new CyclicDependenciesFoundException(visiting);
+            }
+            visiting.push(dependency);
+            checkDependencies(dependency, visiting);
+            visiting.pop();
+        }
+    }
+
+    interface ComponentProvider<T> {
+        T get(Context context);
+
+        List<Class<?>> getDependencies();
+    }
+
+}
